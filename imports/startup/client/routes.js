d7976ceb4de6216057283ac8af6b1e5ec395aab2
Pierre Wieser <pwieser@trychlos.org>
2019-12-30 01:33:55 +0100

    Adding overview page

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -8,7 +8,7 @@ import '/imports/ui/layouts/desktop/desktop.js';
 
 import '/imports/ui/pages/actions/actions.js';
 import '/imports/ui/pages/collect/collect.js';
-//import '/imports/ui/pages/overview/overview.js';
+import '/imports/ui/pages/overview/overview.js';
 //import '/imports/ui/pages/process/process.js';
 //import '/imports/ui/pages/projects/projects.js';
 import '/imports/ui/pages/setup/setup.js';
