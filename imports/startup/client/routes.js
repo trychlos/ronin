054f942b0ae15534e5a96d90af32fe1bfe9e8e4b
Pierre Wieser <pwieser@trychlos.org>
2019-12-30 01:37:03 +0100

    Adding projects page

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -10,7 +10,7 @@ import '/imports/ui/pages/actions/actions.js';
 import '/imports/ui/pages/collect/collect.js';
 import '/imports/ui/pages/overview/overview.js';
 import '/imports/ui/pages/process/process.js';
-//import '/imports/ui/pages/projects/projects.js';
+import '/imports/ui/pages/projects/projects.js';
 import '/imports/ui/pages/setup/setup.js';
 import '/imports/ui/pages/not_found/not_found.js';
 
