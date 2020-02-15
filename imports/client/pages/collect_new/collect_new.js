f94bac46fe2c8f64fd45732891fc9f14bfa82fae
Pierre Wieser <pwieser@trychlos.org>
2020-02-15 08:21:12 +0100

    Cleanup obsolete pages

--- imports/client/pages/collect_new/collect_new.js
+++
@@ -1,8 +0,0 @@
-/*
- * 'collectNew' page.
- *
- *  This page lets the user create new thoughts.
- *
- *  It is intended to be used on (small, smartphones) touchable device only.
- */
-import './collect_new.html';
