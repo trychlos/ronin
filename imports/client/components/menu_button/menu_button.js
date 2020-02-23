8a91276a274fcfd8fed53f121e079f6c0eae33c3
Pierre Wieser <pwieser@trychlos.org>
2020-02-23 20:15:10 +0100

    Display the version number

--- imports/client/components/menu_button/menu_button.js
+++ imports/client/components/menu_button/menu_button.js
@@ -3,13 +3,13 @@
  *
  *  Display a button on the left of the navbar to open the menu.
  */
+import bootbox from 'bootbox/dist/bootbox.all.min.js';
 import './menu_button.html';
 
 Template.menu_button.events({
     'click .js-about'( ev, instance ){
         bootbox.alert({
-            message: "This alert can be dismissed by clicking on the background!",
-            backdrop: true
+            message: 'Ronin version '+Meteor.settings.public.ronin.version
         });
     }
 });
