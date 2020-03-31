37630a020e764f40e5932ebde4f9dd98ec88e2c4
Pierre Wieser <pwieser@trychlos.org>
2020-03-31 15:29:47 +0200

    Fix search path for downloadable files in production

--- imports/api/collections/fs/server/publish.js
+++ imports/api/collections/fs/server/publish.js
@@ -1,14 +1,26 @@
 import { Meteor } from 'meteor/meteor';
 import { FS } from '../fs.js';
 
+// 'dir' is expected to have a trailing slash
+//
+// in production env:
+//  process.env.PWD = /home/ronin/bundle
+//  Mar 31 14:40:05 www8.trychlos.lan start.sh[25384]: [
+//  Mar 31 14:40:05 www8.trychlos.lan start.sh[25384]:   '/home/ronin/bundle/programs/web.browser/app/res/apk/ronin-v20.03.31.2.apk',
+//  Mar 31 14:40:05 www8.trychlos.lan start.sh[25384]:   '/home/ronin/bundle/programs/web.browser.legacy/app/res/apk/ronin-v20.03.31.2.apk',
+//  Mar 31 14:40:05 www8.trychlos.lan start.sh[25384]:   '/home/ronin/bundle/programs/web.cordova/app/res/apk/ronin-v20.03.31.2.apk'
+//  Mar 31 14:40:05 www8.trychlos.lan start.sh[25384]: ]
+//
 Meteor.publish( 'fs.bydir', function( dir ){
     const self = this;
-    const fs = require( 'fs' );
-    const path = process.env.PWD+'/public'+dir;
-    fs.readdir( path, ( e, fnames ) => {
+    const find = require( 'find' );
+    const path = require( 'path' );
+    const rootdir = process.env.PWD + ( Meteor.isProduction ? '/programs/web.browser/app' : '/public' );
+    find.file( /\.apk$/, rootdir+dir, function( fnames ){
         fnames.forEach( f => {
-            self.added( 'FS', f, {
-                url: dir+f
+            const bname = path.basename( f );
+            self.added( 'FS', bname, {
+                url: dir+bname
             });
         });
     });
