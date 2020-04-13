3f1aa504f95cc625c87b4610bac34d1d80dcc184
Pierre Wieser <pwieser@trychlos.org>
2020-04-13 11:52:41 +0200

    plus_button takes care of activating the passed-in action (if any)

--- imports/client/components/plus_button/plus_button.js
+++ imports/client/components/plus_button/plus_button.js
@@ -4,12 +4,24 @@
  *  Display a button on the bottom right of the page.
  *  The positionning is *absolute*.
  *
+ *  If an activable Action is attached to the button via its data context, then
+ *  this action will be automatically activated if the user clicks the button.
+ *  In this case, event propagation will be stopped.
+ *
  * Parameters:
  * - action: maybe:
- *      an Activable action object
- *      a function which returns an Activable action object
+ *   > an activable Action object instance
+ *   > a function which returns an activable Action object instance.
  */
 import './plus_button.html';
+
+Template.plus_button.fn = {
+    // the action passed in the data context may be an Action object instance
+    //  or a function which is expected to return this Action object instance.
+    action( data ){
+        return data && data.action ? ( typeof data.action === 'function' ? data.action() : data.action ) : null;
+    }
+};
 
 Template.plus_button.helpers({
     // not able to make the 'disabled' Bootstrap button attribute working
@@ -17,11 +29,9 @@ Template.plus_button.helpers({
     //  => so use a dedicated class
     classes(){
         let activable = false;
-        if( this.action ){
-            const obj = typeof this.action === 'function' ? this.action() : this.action;
-            if( obj && obj.activable && typeof obj.activable === 'function' ){
-                activable = obj.activable();
-            }
+        const action = Template.plus_button.fn.action( this );
+        if( action ){
+            activable = action.activable();
         }
         return activable ? '': 'disabled';
     }
@@ -29,7 +39,9 @@ Template.plus_button.helpers({
 
 Template.plus_button.events({
     'click button.js-button'( ev, instance ){
-        if( !Meteor.userId()){
+        const action = Template.plus_button.fn.action( instance.data );
+        if( action ){
+            action.activate();
             return false;
         }
     }
