cbc512e3df2f40cebaa559e43df86790b46c134d
Pierre Wieser <pwieser@trychlos.org>
2020-04-30 16:10:06 +0200

    Deploy v20.04.30.1 to production platforms

--- mobile-config.js
+++ mobile-config.js
@@ -9,7 +9,7 @@
 App.info({
     id: 'org.trychlos.ronin',
     name: 'Ronin',
-    version: '20.04.27.1',
+    version: '20.04.30.1',
     description: 'Getting Things Done',
     author: 'Pierre Wieser',
     email: 'pwieser@trychlos.org',
